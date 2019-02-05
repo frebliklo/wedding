735d057e6138457c09f888322463834cd4598bb8
Frederik Klovborg <klovborg@gmail.com>
2019-02-05 18:57:08 +0100

    remove facepaint

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,9 +1,7 @@
-import facepaint from 'facepaint'
-
 const breakpoints = [576, 768, 992, 1200]
 
-export const mq = facepaint(
-  breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+export const mq = breakpoints.map(
+  bp => `@media (min-width: ${bp}px)`
 )
 
 const orange = {
